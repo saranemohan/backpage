5e28e45c1adde5e578350f04fe32d095ce391952
saran <saran@ipixsolutions.com>
2019-09-06 16:00:02 +0530

    remove auth middleware

--- src/routes/route.php
+++ src/routes/route.php
@@ -1,4 +1,4 @@
 <?php
-Route::middleware(['web','auth'])->namespace('Semdevops\Backpage\Controllers')->group(function(){
+Route::middleware(['web'])->namespace('Semdevops\Backpage\Controllers')->group(function(){
     Route::get('route/{route}', 'BackpageController@redirctRoute')->name('routeBack');
 });
